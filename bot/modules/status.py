7e46d57e7d64aa1073d1ca198453c31a338a1796
Hrishikesh <hrishithombare15@gmail.com>
2024-04-16 15:49:26 +0530

    Minor Changes

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -38,10 +38,10 @@ async def mirror_status(ctx):
     if count == 0:
         currentTime = get_readable_time(time() - botStartTime)
         free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-        msg = f"No Active Tasks!\nEach user can get status for his tasks by adding me or user_id after cmd: /{BotCommands.StatusCommand} me"
+        msg = f"<b>Uninstall Switch, and Enjoy your Life.</b>\n\nNo Active Tasks!"
         msg += (
-            f"\n<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {free}"
-            f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UPTIME:</b> {currentTime}"
+            f"\n<b>🖥️ CPU:</b> {cpu_percent()}% | <b>💿 FREE:</b> {free}"
+            f"\n<b>💯 RAM:</b> {virtual_memory().percent}% | <b>⏳ UPTIME:</b> {currentTime}"
         )
         reply_message = await sendMessage(message, msg)
         await auto_delete_message(message, reply_message)
