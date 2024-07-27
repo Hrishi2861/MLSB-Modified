f87a5eef0d3b86bf3b1924ae85a3c1b04ec7c466
Hrishikesh <hrishithombare15@gmail.com>
2024-07-27 12:21:08 +0530

    Minor change in Bot cmds

--- bot/helper/switch_helper/bot_commands.py
+++ bot/helper/switch_helper/bot_commands.py
@@ -5,7 +5,7 @@ class _BotCommands:
         self.StartCommand = f"start{CMD_SUFFIX}"
         self.MirrorCommand = [f"mirror{CMD_SUFFIX}", f"m{CMD_SUFFIX}"]
         self.QbMirrorCommand = [f"qbmirror{CMD_SUFFIX}", f"qbm{CMD_SUFFIX}"]
-        self.YtdlCommand = [f"ytdl{CMD_SUFFIX}", f"y{CMD_SUFFIX}"]
+        self.YtdlCommand = [f"ytdl{CMD_SUFFIX}", f"ytm{CMD_SUFFIX}"]
         self.LeechCommand = [f"leech{CMD_SUFFIX}", f"l{CMD_SUFFIX}"]
         self.QbLeechCommand = [f"qbleech{CMD_SUFFIX}", f"qbl{CMD_SUFFIX}"]
         self.YtdlLeechCommand = [f"ytdlleech{CMD_SUFFIX}", f"ytl{CMD_SUFFIX}"]
